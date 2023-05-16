69e2c545f06ec6e7f5976ec57fda3728db003aff
saschapursche <sascha_pursche@outlook.de>
2023-05-16 20:38:18 +0200

    finished rgbToHex

--- jsLogic-q3.js
+++ jsLogic-q3.js
@@ -1,18 +1,31 @@
 const convertingHexToRgb = () => {
-const userInput = prompt("Enter Hex or RGB value here")
-let splitInput = userInput.match(/.{1,2}/g);
-    if(userInput.length != 6) {
-        throw "Only six-digit hex colors are allowed.";
-    } else {
-    const convertedInput = [
-        parseInt(splitInput[0], 16),
-        parseInt(splitInput[1], 16),
-        parseInt(splitInput[2], 16)
-    ];
-    return convertedInput;
-    };
+    const userInput = prompt("Enter Hex or RGB value here");
+        let splitInput = userInput.match(/.{1,2}/g);
+            if(userInput.length != 6) {
+                alert("Only six-digit hex colors are allowed.");
+            } else {
+            const convertedInput = [
+                parseInt(splitInput[0], 16),
+                parseInt(splitInput[1], 16),
+                parseInt(splitInput[2], 16)
+            ];
+            return convertedInput;
+            };
 };
 convertingHexToRgb();
+
+
+const convertingRgbToHex = () => {
+    const userInput = [prompt("Enter Hex value here")];
+    let converted = userInput[0].match(/\d+/gi).map(Number);
+    return "#" +
+             ((1<<24) +
+             (converted[0]<<16) +
+             (converted[1]<<8) +
+             converted[2]).toString(16).slice(1);
+};
+convertingRgbToHex();
+
 
 
 //learn again, giving some value to another constant
