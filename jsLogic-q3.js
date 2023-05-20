fc924e13abb638744223a2953a917f97ae76a3e2
saschapursche <sascha_pursche@outlook.de>
2023-05-20 14:00:54 +0200

    now function callig works/cleaned up .js file

--- jsLogic-q3.js
+++ jsLogic-q3.js
@@ -2,16 +2,13 @@ const userInput = () => {
     const pattern = /[0-9a-f]{6}/g;
     let input = prompt("Enter Hex or RGB value here")
     if(input.match(pattern)) {
-        console.log("hex", input);
-        convertingHexToRgb(input);
+        return convertingHexToRgb(input);
     } else {
-        console.log("rgb", input);
-        convertingRgbToHex([input]);
+        return convertingRgbToHex([input]);
     };
 }
 
 const convertingHexToRgb = (userInput) => {
-    //const userInput = prompt("Enter Hex value here.");
         let splitInput = userInput.match(/.{1,2}/g);
             if(userInput.length != 6) {
                 alert("Only six-digit hex colors are allowed.");
@@ -24,11 +21,8 @@ const convertingHexToRgb = (userInput) => {
             return convertedInput;
             };
 };
-//convertingHexToRgb();
-
 
 const convertingRgbToHex = (userInput) => {
-    //const userInput = [prompt("Enter RGB value here.")];
         let converted = userInput[0].match(/\d+/gi).map(Number);
         return "#" +
              ((1<<24) +
@@ -36,9 +30,4 @@ const convertingRgbToHex = (userInput) => {
              (converted[1]<<8) +
              converted[2]).toString(16).slice(1);
 };
-//convertingRgbToHex();
 userInput();
-
-//Question 3: Write a function that converts HEX to RGB.
-//Then Make that function auto-dect the formats so that if you enter HEX color format it returns RGB
-//and if you enter RGB color format it returns HEX.
